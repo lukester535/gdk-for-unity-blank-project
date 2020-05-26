6478a2d360242b5ad7f70365d941a729767d96cb
OsAdminLuke <thecookielegends@gmail.com>
2020-05-25 21:56:37 -0500

    AAAAAAAAAAAAAA

---
+++ workers/unity/Assets/HandController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SteamVR;
+
+public class HandController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
